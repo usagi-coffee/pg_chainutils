7f4b35cbdfa8f0891deb269133784c20ad033631
Kamil Jakubus <kamil.jakubus@usagi.coffee>
2024-05-15 04:00:49 +0200

    H160: add parse function

--- src/H160.rs
+++ src/H160.rs
@@ -4,8 +4,12 @@ use pgrx::prelude::*;
 #[allow(non_snake_case)]
 mod H160 {
     use ethers::types::{H160, H256};
-
     use pgrx::prelude::*;
+
+    #[pg_extern(name = "parse", immutable, parallel_safe)]
+    fn parse_h160(h160: &str) -> String {
+        format!("{:#x}", h160.parse::<H160>().unwrap())
+    }
 
     #[pg_extern(immutable, parallel_safe)]
     fn from_abi(h256: &str) -> String {
@@ -27,7 +31,26 @@ mod tests {
 
     #[cfg(not(feature = "no-schema-generation"))]
     #[pg_test]
-    fn h160_test_decode() -> Result<()> {
+    fn h160_parse() -> Result<()> {
+        let data = "1111111111111111111111111111111111111111";
+
+        let decoded = Spi::get_one_with_args::<&str>(
+            "SELECT H160.parse($1);",
+            vec![(
+                PgOid::BuiltIn(PgBuiltInOids::TEXTOID),
+                data.to_string().into_datum(),
+            )],
+        )
+        .unwrap();
+
+        assert_eq!(decoded, Some("0x1111111111111111111111111111111111111111"));
+
+        Ok(())
+    }
+
+    #[cfg(not(feature = "no-schema-generation"))]
+    #[pg_test]
+    fn h160_from_h256() -> Result<()> {
         let address = "0x0000000000000000000000001111111111111111111111111111111111111111";
 
         let decoded = Spi::get_one_with_args::<&str>(
